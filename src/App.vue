9c657a2e83d7ba2ade531ab41559063194f317d4
Monika Stobieniecka <mstobieniecka1@gmail.com>
2018-09-25 17:06:57 +0200

    Added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheNavbar></TheNavbar>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheNavbar from './components/TheNavbar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TheNavbar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <md-toolbar class="md-accent">
+            <h3 class="md-title app-name">Weight Tracker</h3>
+            <nav>
+                <md-button>Statistics</md-button>
+                <md-button>History</md-button>
+            </nav>
+        </md-toolbar>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'TheNavbar',
+}
+</script>
+
+<style scoped>
+.md-title.app-name {
+    flex: 1;
+}
+
+</style>
